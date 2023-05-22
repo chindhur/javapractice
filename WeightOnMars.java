08e1b305cdb62535e8d632992d59390b55853f5a
chindhu Rathnasamy <130512054+chindhur@users.noreply.github.com>
2023-05-21 18:00:20 -0700

    Add files via upload

---
+++ WeightOnMars.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class WeightOnMars {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the weight : ");
+        double weight = scanner.nextDouble();
+        double result = weight*.38;
+        System.out.println("weight on mars:"+result);
+
+        }
+        }
+
+
