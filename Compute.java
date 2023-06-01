6efaefdfb9a59261aceb14b0acb25fa5ed4c1e47
chindhu Rathnasamy <130512054+chindhur@users.noreply.github.com>
2023-06-01 12:29:56 -0700

    Add files via upload

---
+++ Compute.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//Write a Java program that accepts an integer (n) and computes the value of n+nn+nnn.
+
+public class Compute {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=scanner.nextInt();
+        System.out.printf("%d + %d%d + %d%d%d\n", n, n, n, n, n, n);
+
+    }
+
+}
